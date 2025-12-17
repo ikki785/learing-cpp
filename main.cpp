636c07aa22554bad079f42de4143c2f25310518e
ikki785 <mash78589@gmail.com>
2025-12-17 19:56:04 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    string name = "ikki";
-    int age = 14;
-    cout<<"my name is "<<name<<endl;
-    cout<<"i am "<<age<<" year old"<<endl;
+    string name ;
+    cout<<"what is your name"<<endl;
+    cin>>name;
+    cout<<"hi "<<name<<endl;
 }
