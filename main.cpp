9eb57c147b80d543d865e78685912d55bbaf6a83
ikki785 <mash78589@gmail.com>
2025-12-18 19:57:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ void a(){
     int a;
     char b;
     int c;
+    int i = 0;
+    char op ;
     while (true){/*add a loop in calculater*/
+    i++;
     cin>>a;
     cin >>b;
     cin >>c;
@@ -24,6 +27,15 @@ void a(){
             default:
                 cout<<"envialied option fool ";
                 break;
+            
+        }
+        if (i >= 3){
+            cout <<"do you want to contiune"<<endl;
+            cin>>op;
+            if (op == 'n'){
+                break;
+            }
+
         }
     }
     
