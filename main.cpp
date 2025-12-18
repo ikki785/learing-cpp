92fb1402b24310d2defb4dbf5a544aba9d01b7ef
ikki785 <mash78589@gmail.com>
2025-12-18 19:50:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,26 +4,27 @@ void a(){
     int a;
     char b;
     int c;
-    ;
+    while (true){
     cin>>a;
     cin >>b;
     cin >>c;
-    switch (b){
-        case  '+':
-            cout<<a+c<<endl;
-            break;
-        case '-':
-            cout<<a-b<<endl;
-            break;
-        case '*':
-            cout<<a*c<<endl;
-            break;
-        case '/':
-            cout<<a/c<<endl;
-            break;
-        default:
-            cout<<"envialied option fool ";
-            break;
+        switch (b){
+            case  '+':
+                cout<<a+c<<endl;
+                break;
+            case '-':
+                cout<<a-c<<endl;
+                break;
+            case '*':
+                cout<<a*c<<endl;
+                break;
+            case '/':
+                cout<<a/c<<endl;
+                break;
+            default:
+                cout<<"envialied option fool ";
+                break;
+        }
     }
     
 }
