b983b502d2df9af75ce4660d6da7498fb67dfebf
ikki785 <mash78589@gmail.com>
2025-12-20 14:54:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 void ca(){
     int n ;
-    char l = 1+64 ;
+    
     
     cout<<"enter"<<endl;
     cin>>n;
-    
+    char l = n ;
     for (int i = 0;i<n;i++){
-        cout<<string(n,l) << endl;
-        l++;
+        cout<<string(l,'*')<<endl;
+        l--;
     }
 }
 int main(){
