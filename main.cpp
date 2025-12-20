a8d9f41cfe14b5c40a1e29913db5baf74412390f
ikki785 <mash78589@gmail.com>
2025-12-20 14:57:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 void ca(){
     int n ;
     char l = 1 ;
+    char j = '1';
     
     cout<<"enter"<<endl;
     cin>>n;
     
     for (int i = 0;i<n;i++){
-        cout<<string(l,'*')<<endl;
+        cout<<string(l,j)<<endl;
         l++;
+        j++;
+    
     }
+
+
 }
 int main(){
     ca(); 
