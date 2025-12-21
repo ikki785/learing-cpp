2d52cca3dc6f989a86e5d50a313ff80c504b8673
ikki785 <mash78589@gmail.com>
2025-12-21 16:36:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,19 @@ using namespace std;
 void ca(){
     int n ;
     int l = 1 ;
-
+    int j = 1;
     
     cin >>n;
-    int mid = n/2; 
+    l = n - 2;
     
     for (int i = 0;i<n;i++){
-        if (i!=mid){
-            for(int j = 0;j<n;j++){
-                if (j == mid){
-                    cout<<"*";
-                }
-                else{
-                    cout<<" ";    
-                }
-            }
-            cout<<endl;
+        if (j == 1 || j == n){
+            cout<<string(n,'*')<<endl;
+            j++;
         }
         else{
-            cout<<string(n,'*')<<endl;
+            cout<<'*'<<string(l,' ')<<'*'<<endl;
+            j++;
         }
         
     }
