aa456c0c55b5e38d1a3a4c465118833d20d2f2a6
ikki785 <mash78589@gmail.com>
2025-12-22 19:17:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 #include <string>
 using namespace std;
 
-void ca(){
-    int x = 3;
-    int* p = &x;
-    cout<<*p<<endl;
+void ca(int* a,int* b){
+    int temp;
+    temp = *b;
+    *a = *b;
+    *a = temp;
+    
+
+    
+    
 }
 int main(){
-    ca(); 
+    int x =3;
+    int y = 4;
+    cout<<x<<" "<<y<<endl;
+    ca(&x,&y); 
+    cout<<x<<" "<<y<<endl;
     return 0;
 }
