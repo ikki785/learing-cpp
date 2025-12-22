50bc6ba632016f09b53bfebbf870430a62eeaee9
ikki785 <mash78589@gmail.com>
2025-12-22 18:35:48 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 void ca(){
-    int n ;
-    int l = 0 ;
-    int j = 1;
-    
-    cin >>n;
-    l = n;
-    
-    for(int i = 0;i<n;i++){
-        cout<<string(l,' ')<<string(n,'*')<<endl;
-        l--;
-    }
-    
-
-
+    int x = 3;
+    cout<<&x<<endl;
 }
 int main(){
+
     ca(); 
-    return 0;
+    return 100;
 }
