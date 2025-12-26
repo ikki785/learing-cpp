1b0f4ce420b60916c2b9d27864166a17e83033e9
ikki785 <mash78589@gmail.com>
2025-12-26 20:07:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include <string>
 using namespace std;
 class human{
-    
+    private :
+        void ae(char es){
+            string na ;
+            na = es;
+            cout<<na<<endl;
+        }
     public:
         string name;
         int age;
@@ -11,21 +16,23 @@ class human{
             cout<<"do you like big ass"<<endl;
             cin>>op;
             if (op == 'y'||op == 'Y'){
-                for (int i=0;i<101;i++){
+                for (int i=0;i<2;i++){
                     cout<<"big ass"<<endl;
                 }
+                ae('y');
             
             }
             else {
                 cout<<"ok"<<endl;
             }
         }
+    
+
 };
 int main(){
     human human1;
     human1.name = "suman";
     human1.age = 23;
     human1.as();
-
     return 0;
 }
