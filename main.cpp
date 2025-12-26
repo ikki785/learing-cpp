fd06debb34b7d340ba68330436d5d2bcc4a2df5c
ikki785 <mash78589@gmail.com>
2025-12-26 20:03:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int i = 1;
-void fa(){
+class human{
     
-    if (i <= 100){
-        cout<<i<<" "<<"big ass"<<endl;
-        i++;
-        fa();
-    }
-}
+    public:
+        string name;
+        int age;
+        void as(){
+            char op;
+            cout<<"do you like big ass"<<endl;
+            cin>>op;
+            if (op == 'y'||op == 'Y'){
+                for (int i=0;i<101;i++){
+                    cout<<"big ass"<<endl;
+                }
+            
+            }
+            else {
+                cout<<"ok"<<endl;
+            }
+        }
+};
 int main(){
-    string pass ;
-    cout<<"enter password"<<endl;
-    cin >>pass;
-    if (pass != "0"){
-        fa();
-    }
-    
+    human human1;
+    human1.name = "suman";
+    human1.age = 23;
+    human1.as();
+
     return 0;
 }
